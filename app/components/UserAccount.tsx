8f725ecc1ea0455d0bfbbc0589aa5400d57bc36b
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-03 22:30:08 +0530

    outside click done

--- app/components/UserAccount.tsx
+++ app/components/UserAccount.tsx
@@ -1,13 +1,33 @@
 "use client";
 import { FaUserCircle } from "react-icons/fa";
 import { signIn, useSession, signOut } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 
 export const UserAccount = () => {
   const { status, data } = useSession();
   const [isOpen, setIsOpen] = useState(false);
-  console.log(data);
+  const dropDownRef = useRef(null);
+  const dropDownButtonRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (
+        isOpen &&
+        dropDownRef.current &&
+        !dropDownRef.current.contains(e.target) &&
+        dropDownButtonRef.current &&
+        !dropDownButtonRef.current.contains(e.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
   return (
     <div className="relative ">
       <div className="">
@@ -19,14 +39,21 @@ export const UserAccount = () => {
             Login
           </button>
         ) : (
-          <button className="" onClick={() => setIsOpen(!isOpen)}>
+          <button
+            ref={dropDownButtonRef}
+            className=""
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <FaUserCircle size={25} />
           </button>
         )}
       </div>
 
       {isOpen && (
-        <div className="shadow-neutral-800 shadow-md absolute -right-0 mt-2 z-10 bg-neutral-800  flex flex-col gap-2 pt-5">
+        <div
+          ref={dropDownRef}
+          className="shadow-neutral-800 shadow-md absolute -right-0 mt-2 z-10 bg-neutral-800  flex flex-col gap-2 pt-5"
+        >
           <h1 className="px-5">{data?.user?.email}</h1>
           {data?.user.role === "ADMIN" && (
             <Link className="py-2 px-5 hover:bg-neutral-600" href="/dashboard">
