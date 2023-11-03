118922e6ffbf058a1672377272e1ad514405483b
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-03 16:20:01 +0530

    UserAccount rough done

--- app/components/UserAccount.tsx
+++ app/components/UserAccount.tsx
@@ -1,25 +1,38 @@
 "use client";
-
+import { FaUserCircle } from "react-icons/fa";
 import { signIn, useSession, signOut } from "next-auth/react";
+import { useState } from "react";
 
 export const UserAccount = () => {
-  const { status } = useSession();
+  const { status, data } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      {status === "unauthenticated" ? (
-        <button
-          className="px-3 py-2 border-2 border-white"
-          onClick={() => signIn("google")}
-        >
-          Login
-        </button>
-      ) : (
-        <button
-          className="px-3 py-2 border-2 border-white"
-          onClick={() => signOut()}
-        >
-          Logout
-        </button>
+    <div className="relative ">
+      <div className="">
+        {status === "unauthenticated" ? (
+          <button
+            className="px-3 py-2 border-2 border-white"
+            onClick={() => signIn("google")}
+          >
+            Login
+          </button>
+        ) : (
+          <button className="" onClick={() => setIsOpen(!isOpen)}>
+            <FaUserCircle size={25} />
+          </button>
+        )}
+      </div>
+
+      {isOpen && (
+        <div className="shadow-neutral-800 shadow-md absolute -right-0 mt-2 z-10 bg-neutral-800  flex flex-col gap-2 pt-5">
+          <h1 className="px-5">{data?.user?.email}</h1>
+          <button
+            onClick={() => signOut()}
+            className="py-2 hover:bg-neutral-600"
+          >
+            Logout
+          </button>
+        </div>
       )}
     </div>
   );
