7ecd5b07f7349115c1fe78e27bf126a4515e951e
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-06 16:14:20 +0530

    changed to useQuery

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,42 @@
+"use client";
 import { CategorySection } from "../components/CategorySection";
 import { baseUrl } from "../lib/connect";
 import { Category } from "../components/Category";
 import { CategoryChild } from "@/types/types";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "../components/Spinner";
 
-async function getData(url: string) {
-  const response = await fetch(url, {
-    cache: "no-store",
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-    next: { tags: ["categories"] },
+const Page = () => {
+  const {
+    data: categories,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const response = await fetch(`/api/category`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      return response.json();
+    },
   });
-
-  if (!response.ok) {
-    throw new Error("Error fetching data");
-  }
-  return response.json();
-}
-
-const Page = async () => {
-  const categories = await getData(`${baseUrl}api/category`);
+  console.log(categories);
+  if (isError) return null;
   return (
     <div className="p-10 text-white flex flex-col gap-10">
       <CategorySection />
       <div className="flex flex-col gap-2">
         <h1 className="text-xl font-semibold">Categories</h1>
-
-        <div className="flex flex-col gap-2 w-full">
-          {categories.map((category: CategoryChild) => (
-            <Category key={category.id} category={category} />
-          ))}
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div className="flex flex-col gap-2 w-full">
+            {categories.map((category: CategoryChild) => (
+              <Category key={category.id} category={category} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
