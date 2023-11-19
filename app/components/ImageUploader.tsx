282eaaf0d4b9f9704771c1c584689a8a1f9c7d35
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-19 21:53:54 +0530

    uploadthing added

---
+++ app/components/ImageUploader.tsx
@@ -0,0 +1,49 @@
+"use client";
+import type { FileWithPath } from "@uploadthing/react";
+import { useDropzone } from "@uploadthing/react/hooks";
+import { generateClientDropzoneAccept } from "uploadthing/client";
+
+import { useUploadThing } from "../lib/uploadthing";
+import { useCallback, useState } from "react";
+
+export const ImageUploader = () => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { startUpload, permittedFileInfo } = useUploadThing("imageUploader", {
+    onClientUploadComplete: () => {
+      alert("uploaded successfully!");
+    },
+    onUploadError: () => {
+      alert("error occurred while uploading");
+    },
+    onUploadBegin: () => {
+      alert("upload has begun");
+    },
+  });
+
+  const fileTypes = permittedFileInfo?.config
+    ? Object.keys(permittedFileInfo?.config)
+    : [];
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: fileTypes ? generateClientDropzoneAccept(fileTypes) : undefined,
+  });
+
+  return (
+    <div className="p-10 bg-white" {...getRootProps()}>
+      <input {...getInputProps()} />
+      <div>
+        {files.length > 0 && (
+          <button onClick={() => startUpload(files)}>
+            Upload {files.length} files
+          </button>
+        )}
+      </div>
+      Drop files here!
+    </div>
+  );
+};
--- app/sample/page.tsx
+++ app/sample/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { ImageUploader } from "../components/ImageUploader";
+
 const Page = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      sample page
+      <ImageUploader />
     </div>
   );
 };
