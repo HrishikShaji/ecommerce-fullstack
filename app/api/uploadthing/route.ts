1cc8097f086aec195f7cc5b03e6a014b2e20f93f
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-20 15:54:47 +0530

    delete image done

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,28 @@
 import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
+import { UTApi } from "uploadthing/server";
 
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
+
+export async function DELETE(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const fileKey = searchParams.get("fileKey");
+  const utapi = new UTApi();
+
+  if (!fileKey) {
+    return new Response(JSON.stringify("no fileKey"), { status: 500 });
+  }
+
+  try {
+    console.log(fileKey, "its here");
+    await utapi.deleteFiles(fileKey);
+    return new Response(JSON.stringify("deleted"), { status: 500 });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify("error"), { status: 500 });
+  }
+}
--- app/components/ImageUploader.tsx
+++ app/components/ImageUploader.tsx
@@ -6,9 +6,11 @@ import { Spinner } from "./Spinner";
 import Image from "next/image";
 import { UploadFileResponse } from "uploadthing/client";
 import { ImagesData } from "../api/uploadthing/core";
+import { MdDelete } from "react-icons/md";
 
 export const ImageUploader = () => {
   const [files, setFiles] = useState<File[]>([]);
+  const [isImage, setIsImage] = useState(false);
   const [uploadedFiles, setUploadedFiles] = useState<
     UploadFileResponse<ImagesData>[]
   >([]);
@@ -44,43 +46,72 @@ export const ImageUploader = () => {
     const images = Array.from(e.target.files || []);
     setFiles(images);
   };
+
+  const handleDelete = async (id: string) => {
+    console.log(id);
+    await fetch(`api/uploadthing?fileKey=${id}`, {
+      method: "DELETE",
+    });
+  };
   return (
-    <div className="p-10 bg-white">
-      <input
-        multiple
-        onChange={(e) => handleSelect(e)}
-        type="file"
-        ref={ref}
-        className="text-sm text-stone-500
-   file:mr-5 file:py-1 file:px-3 file:border-[1px]
-   file:text-xs file:font-medium
-   file:bg-stone-50 file:text-stone-700
-   hover:file:cursor-pointer hover:file:bg-blue-50
-   hover:file:text-blue-700"
-      />
-      <div>
-        {files.length > 0 && (
-          <>
+    <div className="  flex flex-col gap-2 items-center relative">
+      <div className="flex gap-2 ">
+        <button
+          className="p-2 rounded-md bg-blue-500"
+          onClick={() => setIsImage(!isImage)}
+        >
+          Images
+        </button>
+        <input
+          multiple
+          onChange={(e) => handleSelect(e)}
+          type="file"
+          ref={ref}
+          id="custom-input"
+          hidden
+        />
+        <label
+          htmlFor="custom-input"
+          className="p-2 cursor-pointer rounded-md bg-neutral-500"
+        >
+          Add
+        </label>
+        <div>
+          {files.length > 0 && (
             <button
               onClick={() => startUpload(files)}
-              className="p-2 bg-neutral-500 rounded-md"
+              className="p-2 bg-neutral-400 rounded-md"
             >
-              Upload {files.length} files
-              {isUploading && <Spinner />}
+              {isUploading ? <Spinner /> : `Upload ${files.length} files`}
             </button>
-          </>
-        )}
+          )}
+        </div>
       </div>
-      {uploadedFiles?.map((img) => (
-        <Image
-          key={img.serverData.fileUrl}
-          src={img.serverData.fileUrl}
-          height={1000}
-          width={1000}
-          className="h-[200px] w-[200px] object-cover rounded-md"
-          alt="image"
-        />
-      ))}
+      {isImage && (
+        <div className=" items-center   gap-2 inline-flex  min-h-[100px] origin-center  absolute  top-12 z-10 bg-neutral-700 rounded-md  p-5">
+          {uploadedFiles.length === 0 ? (
+            <div className="">No Images Selected</div>
+          ) : (
+            uploadedFiles?.map((img) => (
+              <div key={img.serverData.fileUrl} className="relative ">
+                <div
+                  className="absolute cursor-pointer top-1 right-1 z-20"
+                  onClick={() => handleDelete(img.key)}
+                >
+                  <MdDelete color="white" />
+                </div>
+                <Image
+                  src={img.serverData.fileUrl}
+                  height={1000}
+                  width={1000}
+                  className="h-[100px] min-w-[100px] object-cover  rounded-md flex-grow"
+                  alt="image"
+                />
+              </div>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 };
