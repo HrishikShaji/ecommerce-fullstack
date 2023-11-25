6e8a78f3a95534c4ec52dcc69ce0ac058915067e
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-25 20:10:48 +0530

    useDatatable added

--- app/components/BillboardSection.tsx
+++ app/components/BillboardSection.tsx
@@ -9,6 +9,8 @@ import Dropdown from "./ui/Dropdown";
 import { SortObjectType } from "@/types/types";
 import { Sort } from "./ui/Sort";
 import { Pagination } from "./ui/Pagination";
+import { useDataTable } from "./ui/useDataTable";
+import { Spinner } from "./Spinner";
 
 const billboardSortValues: SortObjectType[] = [
   {
@@ -22,44 +24,24 @@ const billboardSortValues: SortObjectType[] = [
 ];
 
 const BillBoardSection = () => {
-  const [searchString, setSearchString] = useState("");
-  const [sort, setSort] = useState<SortObjectType>({
-    title: "Latest",
-    value: "LATEST",
-  });
-  const [page, setPage] = useState(1);
-  console.log(sort);
   const {
     data,
-    isError,
     count,
-    refetch: refetchData,
-  } = useGetQuery({
-    endpoint: "billboard",
-    page: page,
-    sort: sort.value,
-    queryKey: "billboards",
-  });
-
-  const {
-    results,
-    isError: isSearchError,
     isLoading,
-    refetch,
-    count: searchCount,
-  } = useSearch({
-    searchString: searchString,
+    page,
+    isError,
+    setPage,
+    setSearchString,
+    setSort,
+  } = useDataTable({
+    endpoint: "billboard",
     section: "billBoard",
-    page: page,
-    sort: sort.value,
+    queryKey: "billboards",
+    initialSortObj: billboardSortValues[0],
   });
-
-  useEffect(() => {
-    refetchData();
-    refetch();
-  }, [searchString, sort, page]);
 
   if (isError) return null;
+
   return (
     <div className="flex flex-col gap-10 w-full">
       <div className="flex flex-col gap-6 px-2">
@@ -74,16 +56,13 @@ const BillBoardSection = () => {
             <Sort setSort={setSort} sortItems={billboardSortValues} />
           </div>
         </div>
-        <Table
-          data={searchString ? results : data}
-          headings={["Billboard", "Date"]}
-        />
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <Table data={data} headings={["Billboard", "Date"]} />
+        )}
       </div>
-      <Pagination
-        count={searchString ? searchCount : count}
-        page={page}
-        setPage={setPage}
-      />
+      <Pagination count={count} page={page} setPage={setPage} />
     </div>
   );
 };
---
+++ app/components/ui/useDataTable.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useGetQuery } from "@/app/lib/queries/customQuery";
+import { useSearch } from "@/app/lib/queries/search";
+import {
+  EndpointType,
+  QueryKey,
+  SectionType,
+  SortObjectType,
+} from "@/types/types";
+import { useEffect, useState } from "react";
+
+interface useDataTableProps {
+  initialSortObj: SortObjectType;
+  endpoint: EndpointType;
+  queryKey: QueryKey;
+  section: SectionType;
+}
+
+export const useDataTable = (props: useDataTableProps) => {
+  const [searchString, setSearchString] = useState("");
+  const [sort, setSort] = useState<SortObjectType>(props.initialSortObj);
+  const [page, setPage] = useState(1);
+  const {
+    data: allData,
+    isError: isDataError,
+    count: dataCount,
+    isLoading: isDataLoading,
+    refetch: refetchData,
+  } = useGetQuery({
+    endpoint: props.endpoint,
+    page: page,
+    sort: sort.value,
+    queryKey: props.queryKey,
+  });
+
+  const {
+    results: searchData,
+    isError: isSearchError,
+    isLoading: isSearchLoading,
+    refetch: refetchResults,
+    count: searchCount,
+  } = useSearch({
+    searchString: searchString,
+    section: props.section,
+    page: page,
+    sort: sort.value,
+  });
+
+  useEffect(() => {
+    refetchData();
+    refetchResults();
+  }, [searchString, sort, page]);
+
+  const data = searchString ? searchData : allData;
+  const count = searchString ? searchCount : dataCount;
+  const isLoading = searchString ? isSearchLoading : isDataLoading;
+  const isError = searchString ? isSearchError : isDataError;
+
+  return {
+    data,
+    count,
+    isLoading,
+    setSort,
+    setSearchString,
+    isError,
+    setPage,
+    page,
+  };
+};
