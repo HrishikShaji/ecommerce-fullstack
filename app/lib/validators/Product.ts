e53992e56163926a8e28a186dae96dfae889308c
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-28 11:18:04 +0530

    validation schema done

--- app/lib/validators/Product.ts
+++ app/lib/validators/Product.ts
@@ -12,18 +12,17 @@ export const productPayload = z.object({
 });
 
 export type ProductPayload = z.infer<typeof productPayload>;
-export const validateProductPayload = (inputs: ProductPayload) => {
-  try {
-    const isValidData = productPayload.parse(inputs);
-    return isValidData;
-  } catch (error) {
-    if (error instanceof ZodError) {
-      if (Object.keys(error.errors).length) {
-        for (const [_key, value] of Object.entries(error.errors)) {
-          throw new Error((value as { message: string }).message);
-        }
-      }
-    }
-    throw error;
-  }
-};
+
+export const updateProductPayload = z.object({
+  id: z.string({ required_error: "id required" }),
+  name: z
+    .string()
+    .min(3, { message: "must be more than 3 characters" })
+    .max(15, { message: "must be less than 15 charaters" }),
+  categoryId: z.string({ required_error: "category is required" }),
+  billboardId: z.string({ required_error: "billboard is required" }),
+  sizeId: z.string({ required_error: "size is required" }),
+  colorId: z.string({ required_error: "color is required" }),
+});
+
+export type UpdateProductPayload = z.infer<typeof updateProductPayload>;
--- app/lib/validators/color.ts
+++ app/lib/validators/color.ts
@@ -9,18 +9,14 @@ export const colorPayload = z.object({
 });
 
 export type ColorPayload = z.infer<typeof colorPayload>;
-export const validateColorPayload = (inputs: ColorPayload) => {
-  try {
-    const isValidData = colorPayload.parse(inputs);
-    return isValidData;
-  } catch (error) {
-    if (error instanceof ZodError) {
-      if (Object.keys(error.errors).length) {
-        for (const [_key, value] of Object.entries(error.errors)) {
-          throw new Error((value as { message: string }).message);
-        }
-      }
-    }
-    throw error;
-  }
-};
+
+export const updateColorPayload = z.object({
+  id: z.string({ required_error: "id required" }),
+  name: z
+    .string()
+    .min(3, { message: "must be more than 3 characters" })
+    .max(15, { message: "must be less than 15 charaters" }),
+  hexCode: z.string().optional(),
+});
+
+export type UpdateColorPayload = z.infer<typeof updateColorPayload>;
