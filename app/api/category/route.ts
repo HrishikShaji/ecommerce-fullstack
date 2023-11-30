602f364a32638f9d441a358c5d2ce8b90faab0fb
HrishikShaji <hrishikminishaji@gmail.com>
2023-11-30 20:25:47 +0530

    billboard api improved

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -4,17 +4,17 @@ import prisma from "@/app/lib/connect";
 import { Category } from "@prisma/client";
 import { CategoryChild } from "@/types/types";
 import { getSortOrder, itemsPerPage, paginateArray } from "@/app/lib/utils";
-import { categoryPayload } from "@/app/lib/validators/category";
+import {
+  categoryPayload,
+  updateCategoryPayload,
+} from "@/app/lib/validators/category";
 
 export async function POST(request: Request) {
   try {
     const payload = await request.json();
     const validatedPayload = categoryPayload.safeParse(payload);
     if (!validatedPayload.success) {
-      const error = validatedPayload.error.errors.map((error) => {
-        return error.message;
-      });
-      return new Response(JSON.stringify(error[0]), {
+      return new Response(JSON.stringify("Invalid Input"), {
         status: 400,
       });
     }
@@ -28,11 +28,12 @@ export async function POST(request: Request) {
         status: 401,
       });
     }
+    const { name, parentId } = validatedPayload.data;
     if (validatedPayload.data.parentId) {
       const category = await prisma.category.create({
         data: {
-          name: validatedPayload.data.name,
-          parentId: validatedPayload.data.parentId,
+          name: name,
+          parentId: parentId,
           userId: user.user.id,
         },
       });
@@ -41,7 +42,7 @@ export async function POST(request: Request) {
 
     const category = await prisma.category.create({
       data: {
-        name: validatedPayload.data.name,
+        name: name,
         userId: user.user.id,
       },
     });
@@ -84,7 +85,6 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const page = Number(searchParams.get("page"));
   const order = getSortOrder(request);
-  console.log("it s=i shere");
   try {
     if (page === 0) {
       const categories = await prisma.category.findMany({
@@ -114,12 +114,10 @@ export async function GET(request: Request) {
       array: withSubCategories,
       page: page,
     });
-    console.log(data);
     return new Response(JSON.stringify({ count, data }), {
       status: 200,
     });
   } catch (error) {
-    console.log(error);
     return new Response(JSON.stringify("error"), { status: 500 });
   }
 }
@@ -145,27 +143,24 @@ export async function DELETE(request: Request) {
 
     return new Response(JSON.stringify("success"), { status: 200 });
   } catch (error) {
-    console.log(error);
     return new Response(JSON.stringify("error"), { status: 500 });
   }
 }
 
 export async function PATCH(request: Request) {
   try {
-    const { name, id } = await request.json();
+    const body = await request.json();
 
     const user = (await getServerSession(authOptions)) as Session;
+    const validatedPayload = updateCategoryPayload.safeParse(body);
+    if (!validatedPayload.success) {
+      return new Response(JSON.stringify("Invalid Input"), { status: 400 });
+    }
 
-    if (!name) {
-      return new Response(JSON.stringify("Wrong input"), { status: 400 });
-    }
-    if (!id) {
-      return new Response(JSON.stringify("wrong input"), { status: 200 });
-    }
     if (user.user.role !== "ADMIN") {
       return new Response(JSON.stringify("unauthorized"), { status: 401 });
     }
-
+    const { id, name } = validatedPayload.data;
     const category = await prisma.category.update({
       where: {
         id: id,
@@ -177,7 +172,6 @@ export async function PATCH(request: Request) {
 
     return new Response(JSON.stringify(category), { status: 200 });
   } catch (error) {
-    console.log(error);
     return new Response(JSON.stringify("error"), { status: 500 });
   }
 }
