c6932ae14a01c2e38ec93649fff024ec84484531
HrishikShaji <hrishikminishaji@gmail.com>
2023-12-05 17:33:48 +0530

    styles inlined input Range

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { ExpRange } from "./components/ui/ExpRange";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-[200px]">
+        <ExpRange />
+      </div>
+    </main>
   );
 }
