7df1ef61f2aaac6cf63058e95db3aa2d7d003859
HrishikShaji <hrishikminishaji@gmail.com>
2023-12-08 23:27:46 +0530

    cart ui changed

--- app/[userId]/cart/page.tsx
+++ app/[userId]/cart/page.tsx
@@ -28,28 +28,42 @@ const Page = () => {
   if (deleteError) return <div>Delete Error</div>;
   if (isLoading) return <div>Loading...</div>;
   return (
-    <div className="text-white">
-      {data.cartItems.map((item: any) => (
-        <div key={item.id} className="flex flex-col gap-2">
-          <Image
-            src={item.product.images[0]}
-            alt="image"
-            height={1000}
-            width={1000}
-            className="h-40 w-40 rounded-md object-cover"
-          />
-          <div>
-            <h1>{item.product.name}</h1>
-            <h1>{item.product.price}</h1>
-          </div>
-          <button
-            onClick={() => remove(item.id)}
-            className="px-3 py-2 rounded-md bg-white text-black"
+    <div className="text-white p-10">
+      <div className="flex flex-col gap-4">
+        {data.cartItems.map((item: any) => (
+          <div
+            key={item.id}
+            className="flex rounded-md bg-neutral-700 p-2 flex-col gap-2 "
           >
-            {isDeleting ? "Deleting" : "Remove"}
-          </button>
-        </div>
-      ))}
+            <div className="flex gap-2 w-full items-center justify-between">
+              <div className="flex gap-2">
+                <Image
+                  src={item.product.images[0]}
+                  alt="image"
+                  height={1000}
+                  width={1000}
+                  className="h-20 w-20 rounded-md object-cover"
+                />
+                <div>
+                  <h1>{item.product.name}</h1>
+                  <h1>{item.product.price}</h1>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <button
+                  onClick={() => remove(item.id)}
+                  className="px-2 py-1 rounded-md bg-white text-black"
+                >
+                  {isDeleting ? "Deleting" : "Remove"}
+                </button>
+                <button className="px-2 py-1 rounded-md bg-white text-black">
+                  Buy
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
