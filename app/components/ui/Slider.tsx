2e862fed79fef4063fcf280548b7d9a40ea82096
HrishikShaji <hrishikminishaji@gmail.com>
2023-12-15 13:24:42 +0530

    slider okay

--- app/components/ui/Slider.tsx
+++ app/components/ui/Slider.tsx
@@ -1,48 +1,67 @@
-import { ReactNode, useEffect, useState } from "react";
+import Image from "next/image";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { BiSolidRightArrow, BiSolidLeftArrow } from "react-icons/bi";
 
 interface SliderProps {
-  children: ReactNode[];
+  slides: string[];
   autoSlide: boolean;
   autoSlideInterval: number;
 }
 
 export const Slider: React.FC<SliderProps> = ({
-  children,
+  slides,
   autoSlide = false,
   autoSlideInterval,
 }) => {
   const [curr, setCurr] = useState(0);
 
   const prev = () =>
-    setCurr((curr) => (curr === 0 ? children.length - 1 : curr - 1));
+    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
   const next = () =>
-    setCurr((curr) => (curr === children.length - 1 ? 0 : curr + 1));
+    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
 
   useEffect(() => {
     if (!autoSlide) return;
     const slideInterval = setInterval(next, autoSlideInterval);
     return () => clearInterval(slideInterval);
-  });
+  }, []);
+
   return (
-    <div className="overflow-hidden  relative">
-      <div
-        className="flex transition-transform ease-out duration-500"
-        style={{ transform: `translateX(-${curr * 100}%)` }}
+    <div className="relative text-black">
+      <button
+        onClick={prev}
+        className="absolute z-10 top-[50%] -translate-y-[50%] -left-10"
       >
-        {children}
-      </div>
-      <div className="absolute inset-0 flex items-center justify-between p-4">
-        <button onClick={prev}>Prev</button>
-        <button onClick={next}>Next</button>
-      </div>
-      <div className="absolute -bottom-2 right-0 left-0 z-10">
+        <BiSolidLeftArrow color="white" />
+      </button>
+      <button
+        onClick={next}
+        className="absolute z-10 right-1 top-[50%] -translate-y-[50%]"
+      >
+        <BiSolidRightArrow />
+      </button>
+      <div className="absolute -bottom-10 right-0 left-0 z-10">
         <div className="flex items-center justify-center gap-2">
-          {children.map((_, i) => (
+          {slides.map((_, i) => (
             <div
               key={i}
               className={`transition-all w-3 h-3 bg-white rounded-full ${
                 curr === i ? "p-4" : "bg-opacity-50"
               }`}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="overflow-hidden  relative h-full">
+        <div
+          className="flex transition-transform  ease-out duration-500"
+          style={{ transform: `translateX(-${curr * 100}%)` }}
+        >
+          {slides.map((item, i) => (
+            <div
+              key={i}
+              className="w-40 h-40 object-cover flex-shrink-0 bg-center"
+              style={{ backgroundImage: `url(${item})` }}
             />
           ))}
         </div>
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,20 +2,29 @@
 
 import { Slider } from "../components/ui/Slider";
 
-const arr = ["red", "green", "yellow"];
+const arr = [
+  "https://images.pexels.com/photos/16756656/pexels-photo-16756656/free-photo-of-black-and-white-photo-of-a-swan-on-a-lake.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+  "https://images.pexels.com/photos/12701974/pexels-photo-12701974.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+  "https://images.pexels.com/photos/8653359/pexels-photo-8653359.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+  "https://images.pexels.com/photos/16756656/pexels-photo-16756656/free-photo-of-black-and-white-photo-of-a-swan-on-a-lake.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+  "https://images.pexels.com/photos/12701974/pexels-photo-12701974.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+  "https://images.pexels.com/photos/8653359/pexels-photo-8653359.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+];
 
 const Page = () => {
   return (
-    <div className="text-white max-w-lg">
-      <Slider autoSlide={true} autoSlideInterval={1000}>
-        {arr.map((item, i) => (
-          <div
-            key={i}
-            className="h-20 w-20"
-            style={{ backgroundColor: item }}
-          ></div>
-        ))}
-      </Slider>
+    <div className="text-white h-screen w-full flex justify-center items-center flex-col gap-10 ">
+      <div className=" h-40  w-40 ">
+        <Slider autoSlide={false} autoSlideInterval={1000} slides={arr} />
+      </div>
+
+      <div className="h-[200px] m-auto w-[200px] border-2 border-white ">
+        <div className="flex gap-2 overflow-hidden">
+          <div className="w-40 h-40 bg-white flex-shrink-0" />
+          <div className="w-40 h-40 bg-white flex-shrink-0" />
+          <div className="w-40 h-40 bg-white flex-shrink-0 " />
+        </div>
+      </div>
     </div>
   );
 };
